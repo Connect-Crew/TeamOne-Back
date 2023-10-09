697524516912e048a69fbceed0064c6f47f8bab9
KangHyeon3315 <son2786w@gmail.com>
2023-10-09 18:13:30 +0900

    chore: 프로젝트 초기화

---
+++ src/main/java/com/connectcrew/teamone/TeamOneApplication.java
@@ -0,0 +1,13 @@
+package com.connectcrew.teamone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connectcrew/teamone/TeamOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectcrew.teamone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
