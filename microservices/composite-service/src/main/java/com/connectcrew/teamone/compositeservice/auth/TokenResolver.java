dc66385315cace2c4d180478f8be2b3e2a228610
KangHyeon3315 <son2786w@gmail.com>
2023-10-24 17:39:45 +0900

    feat: apple의 로그인 기능 구현

--- microservices/composite-service/src/main/java/com/connectcrew/teamone/compositeservice/auth/TokenResolver.java
+++ microservices/composite-service/src/main/java/com/connectcrew/teamone/compositeservice/auth/TokenResolver.java
@@ -2,12 +2,23 @@ package com.connectcrew.teamone.compositeservice.auth;
 
 import com.connectcrew.teamone.api.user.auth.Social;
 import com.connectcrew.teamone.compositeservice.exception.UnauthorizedException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPublicKeySpec;
+import java.util.Base64;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -78,6 +89,56 @@ public class TokenResolver {
     }
 
     private Mono<Auth2User> resolveApple(String token) {
-        return Mono.error(new RuntimeException("Not implemented yet"));
+        return getApplePublicKey(Jwts.parser().build().parseSignedClaims(token).getHeader().getKeyId())
+                .flatMap(publicKey -> {
+                    try {
+                        Jws<Claims> verifiedClaims = Jwts.parser()
+                                .setSigningKey(publicKey)
+                                .build()
+                                .parseSignedClaims(token);
+                        Claims body = verifiedClaims.getPayload();
+                        return Mono.just(new Auth2User(
+                                body.getSubject(),
+                                body.get("email", String.class),
+                                body.get("name", String.class),
+                                null,
+                                Social.APPLE
+                        ));
+                    } catch (Exception ex) {
+                        return Mono.error(new UnauthorizedException(String.format("apple token resolve error - %s", ex.getMessage())));
+                    }
+                });
     }
+
+    private Mono<PublicKey> getApplePublicKey(String kid) {
+        return webClient.get()
+                .retrieve()
+                .bodyToMono(Map.class)
+                .flatMap(response -> {
+                    @SuppressWarnings("unchecked")
+                    var keys = (List<Map<String, Object>>) response.get("keys");
+                    for (Map<String, Object> keyData : keys) {
+                        if (kid.equals(keyData.get("kid"))) {
+                            String n = (String) keyData.get("n");
+                            String e = (String) keyData.get("e");
+                            return Mono.just(createPublicKey(n, e));
+                        }
+                    }
+                    return Mono.error(new RuntimeException("kid " + kid + "에 해당하는 public key를 찾을 수 없습니다."));
+                });
+    }
+
+    private PublicKey createPublicKey(String n, String e) {
+        try {
+            byte[] decodedN = Base64.getUrlDecoder().decode(n);
+            byte[] decodedE = Base64.getUrlDecoder().decode(e);
+
+            RSAPublicKeySpec spec = new RSAPublicKeySpec(new BigInteger(1, decodedN), new BigInteger(1, decodedE));
+            KeyFactory factory = KeyFactory.getInstance("RSA");
+            return factory.generatePublic(spec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+            throw new RuntimeException("RSA public key를 생성할 수 없습니다.", ex);
+        }
+    }
+
 }
