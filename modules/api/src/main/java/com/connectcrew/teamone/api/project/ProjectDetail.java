b212bb042c139f7bc864469f597be060032ab4be
KangHyeon3315 <son2786w@gmail.com>
2023-11-12 14:50:44 +0900

    CON-39 feat: 기술스택을 Enum값이 아니라 문자열로 관리하도록 수정

--- modules/api/src/main/java/com/connectcrew/teamone/api/project/ProjectDetail.java
+++ modules/api/src/main/java/com/connectcrew/teamone/api/project/ProjectDetail.java
@@ -24,7 +24,7 @@ public record ProjectDetail(
         Integer favorite,
         List<RecruitStatus> recruitStatuses,
         List<ProjectMember> members,
-        List<SkillType> skills
+        List<String> skills
 ) {
 
 }
