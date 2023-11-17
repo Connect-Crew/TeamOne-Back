846e79ad05ec0c4d171ee8fac51817f107c73bfd
KangHyeon3315 <son2786w@gmail.com>
2023-11-17 13:40:24 +0900

    CON-47 feat: 모집 현황 정보에 직무 카테고리 추가

--- microservices/composite-service/src/main/java/com/connectcrew/teamone/compositeservice/resposne/RecruitStatusRes.java
+++ microservices/composite-service/src/main/java/com/connectcrew/teamone/compositeservice/resposne/RecruitStatusRes.java
@@ -3,6 +3,7 @@ package com.connectcrew.teamone.compositeservice.resposne;
 import com.connectcrew.teamone.api.project.RecruitStatus;
 
 public record RecruitStatusRes(
+        String category,
         String part,
         String comment,
         Integer current,
@@ -10,6 +11,7 @@ public record RecruitStatusRes(
 ) {
     public RecruitStatusRes(RecruitStatus recruitStatus) {
         this(
+                recruitStatus.part().getCategory().getDescription(),
                 recruitStatus.part().getDescription(),
                 recruitStatus.comment(),
                 recruitStatus.current(),
