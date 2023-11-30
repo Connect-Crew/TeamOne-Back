43341d12ad9af720a9119e2e7d138053565190ff
KangHyeon3315 <son2786w@gmail.com>
2023-11-30 22:32:01 +0900

    CON-51 파일 누락 추가

---
+++ microservices/gateway/src/main/java/com/connectcrew/teamone/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.connectcrew.teamone.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.connectcrew.teamone")
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
