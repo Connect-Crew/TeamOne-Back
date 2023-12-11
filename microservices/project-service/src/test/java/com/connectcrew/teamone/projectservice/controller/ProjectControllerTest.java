b50f8b52ce59d3e1f4c74768ed2ae711d9f83e90
KangHyeon3315 <son2786w@gmail.com>
2023-12-11 21:13:52 +0900

    CON-60 [Back] Project Service 테스트코드 수정

--- microservices/project-service/src/test/java/com/connectcrew/teamone/projectservice/controller/ProjectControllerTest.java
+++ microservices/project-service/src/test/java/com/connectcrew/teamone/projectservice/controller/ProjectControllerTest.java
@@ -1,25 +1,27 @@
 package com.connectcrew.teamone.projectservice.controller;
 
 import com.connectcrew.teamone.api.exception.ErrorInfo;
-import com.connectcrew.teamone.api.project.ApplyInput;
 import com.connectcrew.teamone.api.project.ReportInput;
-import com.connectcrew.teamone.api.project.values.MemberPart;
-import com.connectcrew.teamone.projectservice.entity.Apply;
-import com.connectcrew.teamone.projectservice.entity.Part;
-import com.connectcrew.teamone.projectservice.entity.Report;
-import com.connectcrew.teamone.projectservice.repository.*;
+import com.connectcrew.teamone.projectservice.config.TestConfig;
+import com.connectcrew.teamone.projectservice.member.adapter.out.persistence.repository.ApplyRepository;
+import com.connectcrew.teamone.projectservice.member.adapter.out.persistence.repository.MemberRepository;
+import com.connectcrew.teamone.projectservice.project.adapter.in.web.ProjectController;
+import com.connectcrew.teamone.projectservice.project.adapter.out.persistence.entity.ReportEntity;
+import com.connectcrew.teamone.projectservice.project.adapter.out.persistence.repository.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
+@Import(TestConfig.class)
 @ExtendWith(SpringExtension.class)
 @WebFluxTest(ProjectController.class)
 class ProjectControllerTest {
@@ -27,214 +29,42 @@ class ProjectControllerTest {
     @Autowired
     private WebTestClient webTestClient;
 
-    @MockBean
+    @Autowired
     private ProjectRepository projectRepository;
 
-    @MockBean
+    @Autowired
     private BannerRepository bannerRepository;
 
-    @MockBean
+    @Autowired
     private PartRepository partRepository;
 
-    @MockBean
+    @Autowired
     private ApplyRepository applyRepository;
 
-    @MockBean
+    @Autowired
     private SkillRepository skillRepository;
 
-    @MockBean
+    @Autowired
     private CategoryRepository categoryRepository;
 
-    @MockBean
+    @Autowired
     private MemberRepository memberRepository;
 
-    @MockBean
+    @Autowired
     private ReportRepository reportRepository;
 
-    @MockBean
+    @Autowired
     private CustomRepository customRepository;
-
-    // apply
-    @Test
-    void applyTest() {
-        Part part = Part.builder()
-                .id(1L)
-                .project(1L)
-                .partCategory("testCategory")
-                .part("testPart")
-                .comment("testComment")
-                .collected(0)
-                .targetCollect(2)
-                .build();
-        when(projectRepository.existsById(anyLong())).thenReturn(Mono.just(true));
-        when(partRepository.findByProjectAndPart(anyLong(), anyString())).thenReturn(Mono.just(part));
-        when(memberRepository.existsByPartIdAndUser(anyLong(), anyLong())).thenReturn(Mono.just(false));
-        when(applyRepository.existsByPartIdAndUser(anyLong(), anyLong())).thenReturn(Mono.just(false));
-        when(applyRepository.save(any(Apply.class))).thenReturn(Mono.just(Apply.builder().build()));
-
-        webTestClient.post()
-                .uri("/apply")
-                .bodyValue(new ApplyInput(
-                        1L,
-                        1L,
-                        MemberPart.ADVERTISEMENT_DESIGNER,
-                        "testComment"
-                ))
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(Boolean.class);
-    }
-
-    @Test
-    void applyNotfoundProjectTest() {
-        Part part = Part.builder()
-                .id(1L)
-                .project(1L)
-                .partCategory("testCategory")
-                .part("testPart")
-                .comment("testComment")
-                .collected(0)
-                .targetCollect(2)
-                .build();
-        when(projectRepository.existsById(anyLong())).thenReturn(Mono.just(false));
-        when(partRepository.findByProjectAndPart(anyLong(), anyString())).thenReturn(Mono.just(part));
-        when(memberRepository.existsByPartIdAndUser(anyLong(), anyLong())).thenReturn(Mono.just(false));
-        when(applyRepository.existsByPartIdAndUser(anyLong(), anyLong())).thenReturn(Mono.just(false));
-        when(applyRepository.save(any(Apply.class))).thenReturn(Mono.just(Apply.builder().build()));
-
-        webTestClient.post()
-                .uri("/apply")
-                .bodyValue(new ApplyInput(
-                        1L,
-                        1L,
-                        MemberPart.ADVERTISEMENT_DESIGNER,
-                        "testComment"
-                ))
-                .exchange()
-                .expectStatus().isNotFound()
-                .expectBody(ErrorInfo.class);
-    }
-
-    @Test
-    void applyNotfoundPartTest() {
-        when(projectRepository.existsById(anyLong())).thenReturn(Mono.just(true));
-        when(partRepository.findByProjectAndPart(anyLong(), anyString())).thenReturn(Mono.empty());
-        when(memberRepository.existsByPartIdAndUser(anyLong(), anyLong())).thenReturn(Mono.just(false));
-        when(applyRepository.existsByPartIdAndUser(anyLong(), anyLong())).thenReturn(Mono.just(false));
-        when(applyRepository.save(any(Apply.class))).thenReturn(Mono.just(Apply.builder().build()));
-
-        webTestClient.post()
-                .uri("/apply")
-                .bodyValue(new ApplyInput(
-                        1L,
-                        1L,
-                        MemberPart.ADVERTISEMENT_DESIGNER,
-                        "testComment"
-                ))
-                .exchange()
-                .expectStatus().isNotFound()
-                .expectBody(ErrorInfo.class);
-    }
-
-    @Test
-    void applyAlreadyCollectedTest() {
-        Part part = Part.builder()
-                .id(1L)
-                .project(1L)
-                .partCategory("testCategory")
-                .part("testPart")
-                .comment("testComment")
-                .collected(2)
-                .targetCollect(2)
-                .build();
-        when(projectRepository.existsById(anyLong())).thenReturn(Mono.just(true));
-        when(partRepository.findByProjectAndPart(anyLong(), anyString())).thenReturn(Mono.just(part));
-        when(memberRepository.existsByPartIdAndUser(anyLong(), anyLong())).thenReturn(Mono.just(false));
-        when(applyRepository.existsByPartIdAndUser(anyLong(), anyLong())).thenReturn(Mono.just(false));
-        when(applyRepository.save(any(Apply.class))).thenReturn(Mono.just(Apply.builder().build()));
-
-        webTestClient.post()
-                .uri("/apply")
-                .bodyValue(new ApplyInput(
-                        1L,
-                        1L,
-                        MemberPart.ADVERTISEMENT_DESIGNER,
-                        "testComment"
-                ))
-                .exchange()
-                .expectStatus().isBadRequest()
-                .expectBody(ErrorInfo.class);
-    }
-
-    @Test
-    void applyAlreadyMemberTest() {
-        Part part = Part.builder()
-                .id(1L)
-                .project(1L)
-                .partCategory("testCategory")
-                .part("testPart")
-                .comment("testComment")
-                .collected(0)
-                .targetCollect(2)
-                .build();
-        when(projectRepository.existsById(anyLong())).thenReturn(Mono.just(true));
-        when(partRepository.findByProjectAndPart(anyLong(), anyString())).thenReturn(Mono.just(part));
-        when(memberRepository.existsByPartIdAndUser(anyLong(), anyLong())).thenReturn(Mono.just(true));
-        when(applyRepository.existsByPartIdAndUser(anyLong(), anyLong())).thenReturn(Mono.just(false));
-        when(applyRepository.save(any(Apply.class))).thenReturn(Mono.just(Apply.builder().build()));
-
-        webTestClient.post()
-                .uri("/apply")
-                .bodyValue(new ApplyInput(
-                        1L,
-                        1L,
-                        MemberPart.ADVERTISEMENT_DESIGNER,
-                        "testComment"
-                ))
-                .exchange()
-                .expectStatus().isBadRequest()
-                .expectBody(ErrorInfo.class);
-    }
-
-    @Test
-    void applyAlreadyApplyTest() {
-        Part part = Part.builder()
-                .id(1L)
-                .project(1L)
-                .partCategory("testCategory")
-                .part("testPart")
-                .comment("testComment")
-                .collected(0)
-                .targetCollect(2)
-                .build();
-        when(projectRepository.existsById(anyLong())).thenReturn(Mono.just(true));
-        when(partRepository.findByProjectAndPart(anyLong(), anyString())).thenReturn(Mono.just(part));
-        when(memberRepository.existsByPartIdAndUser(anyLong(), anyLong())).thenReturn(Mono.just(false));
-        when(applyRepository.existsByPartIdAndUser(anyLong(), anyLong())).thenReturn(Mono.just(true));
-        when(applyRepository.save(any(Apply.class))).thenReturn(Mono.just(Apply.builder().build()));
-
-        webTestClient.post()
-                .uri("/apply")
-                .bodyValue(new ApplyInput(
-                        1L,
-                        1L,
-                        MemberPart.ADVERTISEMENT_DESIGNER,
-                        "testComment"
-                ))
-                .exchange()
-                .expectStatus().isBadRequest()
-                .expectBody(ErrorInfo.class);
-    }
 
     // report
     @Test
     void reportTest() {
         when(projectRepository.existsById(anyLong())).thenReturn(Mono.just(true));
         when(reportRepository.existsByProjectAndUser(anyLong(), anyLong())).thenReturn(Mono.just(false));
-        when(reportRepository.save(any(Report.class))).thenReturn(Mono.just(Report.builder().build()));
+        when(reportRepository.save(any(ReportEntity.class))).thenReturn(Mono.just(ReportEntity.builder().build()));
 
         webTestClient.post()
-                .uri("/report")
+                .uri("/project/report")
                 .bodyValue(new ReportInput(
                         1L,
                         1L,
@@ -249,10 +79,10 @@ class ProjectControllerTest {
     void reportNotfoundProjectTest() {
         when(projectRepository.existsById(anyLong())).thenReturn(Mono.just(false));
         when(reportRepository.existsByProjectAndUser(anyLong(), anyLong())).thenReturn(Mono.just(false));
-        when(reportRepository.save(any(Report.class))).thenReturn(Mono.just(Report.builder().build()));
+        when(reportRepository.save(any(ReportEntity.class))).thenReturn(Mono.just(ReportEntity.builder().build()));
 
         webTestClient.post()
-                .uri("/report")
+                .uri("/project/report")
                 .bodyValue(new ReportInput(
                         1L,
                         1L,
@@ -267,10 +97,10 @@ class ProjectControllerTest {
     void reportAlreadyReportTest() {
         when(projectRepository.existsById(anyLong())).thenReturn(Mono.just(true));
         when(reportRepository.existsByProjectAndUser(anyLong(), anyLong())).thenReturn(Mono.just(true));
-        when(reportRepository.save(any(Report.class))).thenReturn(Mono.just(Report.builder().build()));
+        when(reportRepository.save(any(ReportEntity.class))).thenReturn(Mono.just(ReportEntity.builder().build()));
 
         webTestClient.post()
-                .uri("/report")
+                .uri("/project/report")
                 .bodyValue(new ReportInput(
                         1L,
                         1L,
