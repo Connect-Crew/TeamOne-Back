a4f9ab7fb54c237ef49a93966d0d0042b57ba942
KangHyeon3315 <son2786w@gmail.com>
2023-12-17 22:52:31 +0900

    hotfix 배포중 오류 수정

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,20 @@
 docker-compose stop composite
 docker-compose stop user
 docker-compose stop project
+docker-compose stop chat
+docker-compose stop gateway
 
 docker rm teamone-composite
 docker rm teamone-user
 docker rm teamone-project
+docker rm teamone-chat
+docker rm teamone-gateway
 
 docker rmi teamone-back-composite:latest
 docker rmi teamone-back-user:latest
 docker rmi teamone-back-project:latest
+docker rmi teamone-back-chat:latest
+docker rmi teamone-back-gateway:latest
 
 ./gradlew clean
 ./gradlew build
